82dbd86723920d1373ec8b928a4ef6636364152f
Code Palace <codepalace@fastmail.se>
2022-02-05 13:27:10 +0100

    Here we go

---
+++ ButtonEffects/ButtonPressActions.swift
@@ -0,0 +1,39 @@
+//
+//  ButtonPressActions.swift
+//  ButtonEffects
+//
+//  Created by Federico on 05/02/2022.
+//
+
+import Foundation
+import SwiftUI
+
+//  Written by SerialCoder.dev
+struct ButtonPress: ViewModifier {
+    var onPress: () -> Void
+    var onRelease: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ _ in
+                        onPress()
+                    })
+                    .onEnded({ _ in
+                        onRelease()
+                    })
+            )
+    }
+}
+
+//  Written by SerialCoder.dev
+extension View {
+    func pressEvents(onPress: @escaping (() -> Void), onRelease: @escaping (() -> Void)) -> some View {
+        modifier(ButtonPress(onPress: {
+            onPress()
+        }, onRelease: {
+            onRelease()
+        }))
+    }
+}
--- ButtonEffects/ContentView.swift
+++ ButtonEffects/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPressed = false
+    @State private var counter = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(counter)")
+                .font(.system(size: 60, weight: .bold))
+                .opacity(isPressed ? 0.4 : 1.0)
+                .scaleEffect(isPressed ? 1.2 : 1.0)
+            
+            ZStack {
+                Capsule()
+                    .frame(width: 150, height: 50)
+                    .foregroundColor(.blue)
+                
+                Text("Tap me!")
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .scaleEffect(isPressed ? 1.05 : 1.0)
+            .opacity(isPressed ? 0.6 : 1.0)
+            .onTapGesture {
+                counter += 1
+            }
+            .pressEvents {
+                // On press
+                withAnimation(.easeInOut(duration: 0.1)) {
+                    isPressed = true
+                }
+            } onRelease: {
+                withAnimation {
+                    isPressed = false
+                }
+            }
+        }
     }
 }
 
