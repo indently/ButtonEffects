7715b0634518fc4aea96138136bb8f016e2cc7e3
Code Palace <codepalace@fastmail.se>
2022-02-05 12:56:57 +0100

    Initial Commit

---
+++ ButtonEffects/ButtonEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonEffectsApp.swift
+//  ButtonEffects
+//
+//  Created by Federico on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ButtonEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonEffects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonEffects
+//
+//  Created by Federico on 05/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
